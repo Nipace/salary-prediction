40081014cb912cd334f5e4dbb5d50a07ab685a57
Nipesh <nipesh12pant@outlook.com>
2021-01-08 23:10:35 +0545

    knn

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template,jsonify
 import pickle
 
 app = Flask(__name__)
@@ -13,6 +13,16 @@ def predict():
     android_developer = 0 
     laravel_developer = 0 
     web_developer = 0
+    react_native_developer=0
+    flutter_developer=0
+    nodejs_developer=0
+    database_administrator=0
+    php_developer=0
+    net_developer=0
+    kotlin_developer=0
+    full_stack_developer=0
+    digital_marketing=0
+    network_administrator=0
     experience = 0
     if (request.form['job'] == "Laravel Developer"):
         laravel_developer = 1
@@ -20,13 +30,35 @@ def predict():
         android_developer = 1
     if (request.form['job'] == "Web Developer"):
         web_developer = 1
+    if (request.form['job'] == "React native developer"):
+        react_native_developer = 1
+    if (request.form['job'] == "Flutter developer"):
+        flutter_developer = 1
+    if (request.form['job'] == "Nodejs developer"):
+        nodejs_developer = 1
+    if (request.form['job'] == "Database administrator"):
+        database_administrator = 1
+    if (request.form['job'] == "Php developer"):
+        php_developer = 1
+    if (request.form['job'] == ".NET Developer"):
+        net_developer = 1
+    if (request.form['job'] == "Kotlin Developer"):
+        kotlin_developer = 1
+    if (request.form['job'] == "Full Stack Developerr"):
+        full_stack_developer = 1
+    if (request.form['job'] == "Digital Marketing"):
+        digital_marketing = 1
+    if (request.form['job'] == "Network Administrator"):
+        network_administrator = 1
     else:
         laravel_developer = 0
     experience = float(request.form['experience'])
    
-    prediction = model.predict([[android_developer,laravel_developer,web_developer,experience]])
+    prediction = model.predict([[android_developer,laravel_developer,web_developer,react_native_developer,
+    flutter_developer,nodejs_developer,database_administrator,php_developer,net_developer,
+    kotlin_developer,full_stack_developer,digital_marketing,network_administrator,experience]])
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text = "Salary Prediction is Rs {}".format(output))
+    return jsonify({'salar_prediction': str(output)}), 201
     '''
     
 
--- salary_prediction.py
+++ salary_prediction.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import pickle
-
+from logistic_regression import KNN
 #importing dataset 
-dataset = pd.read_csv('Salary_Data.csv')
 
-#categorical data
+dataset = pd.read_csv('Salary_Data4.csv')
+
+# #categorical data
 dataset_dummy = pd.get_dummies(dataset["Position"])
 
 dataset_dummy = pd.concat([dataset_dummy,dataset],axis=1)
@@ -17,18 +18,32 @@ dataset_dummy.drop(["Position"], inplace = True, axis =1)
 X = dataset_dummy.iloc[:, :-1].values
 y = dataset_dummy.iloc[:, -1].values
 
-#splitting the dataset into the training set nad test set
+# #splitting the dataset into the training set nad test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train , y_test = train_test_split(X, y , test_size = 1/3, random_state = 0)
 
-#fitting simple linear regression to training sets
-from sklearn.linear_model import LogisticRegression 
-regressor = LogisticRegression(max_iter=1000)
-regressor.fit(X_train, y_train)
+# #fitting simple linear regression to training sets
+# from sklearn.linear_model import LinearRegression 
+# regressor = LinearRegression()
 
-#predicting the test set results
-y_pred = regressor.predict(X_test)
+# regressor.fit(X_train, y_train)
+# #model = LineaRegression( learning_rate = 0.001, n_iters = 1000000 )
+# #model.fit(X_train, y_train) 
+# #predicting the test set results
+# y_pred = regressor.predict(X_test)
+# y_pred1 = clf.predict(X_test)
 
-pickle.dump(regressor, open('model.pkl','wb'))    
+k = 3
+clf = KNN(k=k)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+def accuracy(y_true, y_predi):
+    accuracy = np.sum(y_true == y_predi) / len(y_true)
+    return accuracy
+
+mse = accuracy(y_test, y_pred)
+print("MSE:", mse)
+pickle.dump(clf, open('model.pkl','wb'))    
 
 
